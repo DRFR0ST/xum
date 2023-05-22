b3d5ea9a752719787d99e102a414996e8e73bdbc
Mike Eling <mike.eling97@gmail.com>
2023-05-22 23:04:22 +0200

    :bookmark: Bump version to 1.0.0-alpha.8

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const program = new Command('xum');
 
 program
   .description('Extremely Universal Manager - A cli app that unifies all Node package managers.')
-  .version("1.0.0-alpha.7");
+  .version("1.0.0-alpha.8");
 
 program.command('info').description('Print detected package manager').action(infoHandler);
 
